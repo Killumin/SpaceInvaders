25f086af3f231bf6aa565b039af622fe4623878e
HoroTheWiseWolf <tigeisbauer@gmail.com>
2018-09-11 12:42:39 +0200

    GUI.java und GameOver.java auf den stand von gestern gebracht

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -8,43 +8,66 @@ import javafx.stage.Stage;
 public class GUI extends Application{
 
     Stage window;
-    Scene menu;
+    Scene menuScene;
+    Scene gameScene;
     Button startGameGui;
-    StackPane layout;
+    Button returnMenu;
+    StackPane menuLayout;
+    StackPane gameLayout;
     Label ueberschrift;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         
-        // Buttons
-        startGameGui = new Button("Start Game");
-        startGameGui.setOnAction(e -> System.out.println("Hello"));
-        
-        //Labels
-        ueberschrift = new Label();
-        ueberschrift.setText("Welcome to SpaceInvaders!");
-        
-        //Layout
-        layout = new StackPane();
-        layout.getChildren().addAll(ueberschrift, startGameGui);
-        layout.setAlignment(Pos.CENTER);
-        
-        //Scene 
-        menu = new Scene(layout,600,600);
-
         // Window 
-        window.setScene(menu);
+        window.setScene(menueScene());
         window.setTitle("SpaceInvader");
         window.show();
-        
     }
 
-    public void setScene(Scene scene) {
-        window.setScene(scene);
+    private Scene menueScene(){
+        //Menu  Buttons
+        startGameGui = new Button("Start Game");
+        startGameGui.setOnAction(e -> {
+            window.setScene(gameScene());
+            GameOver.endScreen();
+        });
+             
+        //Menu Labels
+        ueberschrift = new Label();
+        ueberschrift.setText("Welcome to SpaceInvaders!");
+
+
+        //Menu Layout
+        menuLayout = new StackPane();
+        menuLayout.getChildren().addAll(ueberschrift, startGameGui);
+        menuLayout.setAlignment(Pos.CENTER);
+
+        // Menu Scene 
+        int width = 600;
+        menuScene = new Scene(menuLayout, width, width);
+
+        return menuScene;
     }
 
-    public Scene getScene(){
-        return menu;
+    
+    private Scene gameScene(){
+        //Game Button 
+        //returnMenu = new Button("Return to Menu!");
+        //returnMenu.setOnAction(e -> window.setScene(menuScene));
+
+        //Game Layout
+        gameLayout = new StackPane();
+        //gameLayout.getChildren().add(returnMenu);
+        //gameLayout.setAlignment(Pos.BOTTOM_CENTER);
+
+        // Game Scene 
+        gameScene = new Scene(gameLayout);
+        window.setFullScreenExitHint("");
+        window.setScene(gameScene);
+        window.setFullScreen(true);
+
+        return gameScene;
     }
 }
--- src/main/java/GameOver.java
+++ src/main/java/GameOver.java
@@ -1,4 +1,37 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class GameOver {
+
+    public static void endScreen() {
+        // Window
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Game Over!");
+
+        // Labels
+
+        // Buttons
+        Button retry = new Button("Retry!");
+        Button menu = new Button("Back to Menu!");
+        Button close = new Button("Close (for now !!)");
+        close.setOnAction(e -> window.close());
+
+        // Layout
+        VBox layout = new VBox(20);
+        layout.getChildren().addAll(retry,menu);
+        layout.setAlignment(Pos.BASELINE_CENTER);
+
+        // Scene 
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
 
 }
