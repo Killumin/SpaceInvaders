2b9dd00b89e49735885ce25b3bfd8260d4abcda7
Christian Bauer <christian.b1998@web.de>
2018-09-12 20:41:02 +0200

    da

--- src/main/java/JFXAnsatz/Starship.java
+++ src/main/java/JFXAnsatz/Starship.java
@@ -1,36 +1,45 @@
 package JFXAnsatz;
 
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
-public class Starship extends Rectangle {
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-//	public Starship(InputStream arg0, double arg1, double arg2, boolean arg3, boolean arg4) {
-//		super(arg0, arg1, arg2, arg3, arg4);
-//		// TODO Auto-generated constructor stub
-//	}
+public class Starship extends Image {
 
-	public Starship(int x, int y, int w, int h, String type, Color color) {
-        super(w, h, color);
+	public Starship(InputStream arg0, double arg1, double arg2, boolean arg3, boolean arg4) throws FileNotFoundException {
+		super(new FileInputStream("C:\\Users\\Killumi\\Pictures\\Starship.png"), arg1, arg2, arg3, arg4);
+	}
 
-        setTranslateX(x);
-        setTranslateY(y);
+//	public Starship(int x, int y, int w, int h, String type, Color color) {
+//        super(w, h, color);
+//
+//        setTranslateX(x);
+//        setTranslateY(y);
+//    }
+
+    public ImageView init() {
+    	
+    	ImageView RealShip = new ImageView(this);
+    	return RealShip;
+    }
+    
+    void moveLeft(ImageView iv) {
+        iv.setX(iv.getX() - 5);
     }
 
-    void moveLeft() {
-        setTranslateX(getTranslateX() - 5);
+    void moveRight(ImageView iv) {
+    	iv.setX(iv.getX() + 5);
     }
 
-    void moveRight() {
-        setTranslateX(getTranslateX() + 5);
+    void moveUp(ImageView iv) {
+    	iv.setY(iv.getY() + 5);
     }
 
-    void moveUp() {
-        setTranslateY(getTranslateY() - 5);
-    }
-
-    void moveDown() {
-        setTranslateY(getTranslateY() + 5);
+    void moveDown(ImageView iv) {
+    	iv.setY(iv.getY() + 5);
     }
 	
 
